4c5b2641022f5cfc0a203d9dbd282fb95fb4b769
DK\mao <mao@sitecore.net>
2018-12-07 17:34:45 +0200

    Add processor which publishes shared blob field values to web.

---
+++ src/Sitecore.Support.298205/Publishing/Pipelines/PublishItem/UpdateBlobs.cs
@@ -0,0 +1,47 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Publishing;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Sitecore.Support.Publishing.Pipelines.PublishItem
+{
+  public class UpdateBlobs : PublishItemProcessor
+  {
+    private static MethodInfo _copyBlobFields;
+
+    static UpdateBlobs()
+    {
+      _copyBlobFields = typeof(Sitecore.Publishing.PublishHelper)
+          .GetMethod("CopyBlobFields", BindingFlags.NonPublic | BindingFlags.Instance);
+    }
+
+    public override void Process(PublishItemContext context)
+    {
+      if (context.Action != PublishAction.PublishSharedFields)
+      {
+        return;
+      }
+
+      var sourceItem = context.PublishHelper.GetSourceItem(context.ItemId);
+      if (sourceItem == null)
+      {
+        return;
+      }
+
+      var targetItem = context.PublishHelper.GetTargetItem(context.ItemId);
+      if (targetItem != null)
+      {
+        using (new SecurityDisabler())
+        using (new EditContext(targetItem))
+        {
+          _copyBlobFields.Invoke(context.PublishHelper, new object[] { sourceItem, targetItem });
+        }
+      }
+    }
+  }
+}
